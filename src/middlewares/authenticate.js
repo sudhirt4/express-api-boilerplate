4433e9fcc5e7df8a142a6967e7a1bb0126882238
Sudhir Shrestha <sudhirshrestha@live.com>
2018-02-04 10:08:06 +0545

    Added common submodule

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,11 +1,11 @@
-import Boom from 'boom';
+import Boom from "boom";
 
-import * as TokenUtils from '../utils/token';
-import MESSAGES from '../constants/messages';
-import { AuthDevice } from '../models';
+import * as TokenUtils from "../utils/token";
+import MESSAGES from "../constants/messages";
+import { AuthDevice } from "../models";
 
 export async function authenticate(req, res, next) {
-  const accessToken = req.get('authorization');
+  const accessToken = req.get("authorization");
   try {
     const decoded = TokenUtils.verifyAccessToken(accessToken);
     const deviceId = decoded.data.id;
@@ -13,7 +13,7 @@ export async function authenticate(req, res, next) {
     res.locals.user = decoded.data.payload;
     res.locals.deviceId = deviceId;
 
-    await AuthDevice.update({ lastUsedAt: new Date() }, { where: { id: deviceId } });
+    AuthDevice.update({ lastUsedAt: new Date() }, { where: { id: deviceId } });
 
     next();
   } catch (err) {
