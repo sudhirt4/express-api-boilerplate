83c0001257397b1ed1dc3dd8b1e64067c0dc9200
Sudhir Shrestha <sudhirshrestha@live.com>
2018-03-04 21:25:00 +0545

    Updated env config

--- src/config/db.js
+++ src/config/db.js
@@ -1,26 +1,10 @@
-require('babel-register');
-require('../env');
+require("babel-register");
+require("../env");
 
 module.exports = {
-  development: {
-    username: process.env.DEV_DATABASE_USER,
-    password: process.env.DEV_DATABASE_PASSWORD,
-    database: process.env.DEV_DATABASE_NAME,
-    host: process.env.DEV_DATABASE_HOST,
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.TEST_DATABASE_USER,
-    password: process.env.TEST_DATABASE_PASSWORD,
-    database: process.env.TEST_DATABASE_NAME,
-    host: process.env.TEST_DATABASE_HOST,
-    dialect: 'postgres'
-  },
-  production: {
-    username: process.env.PROD_DATABASE_USER,
-    password: process.env.PROD_DATABASE_PASSWORD,
-    database: process.env.PROD_DATABASE_NAME,
-    host: process.env.PROD_DATABASE_HOST,
-    dialect: 'postgres'
-  }
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: "postgres"
 };
